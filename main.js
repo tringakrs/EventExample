1812a696db5fc43c49daec3586e743cfcaf15d68
Tringa Krasniqi <tringe.krs#gmail.com>
2023-02-18 12:07:40 +0100

    even numbers

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 const express=require('express');
+
+/*
+Krijo nje app node/express ku thirret moduli event . krijo/thirr eventin me emer “runEvent_1” i cili kur thirreth  return “Hello from events”. 
+Krijo nje evnet tjeter ku ne parameter pranon nje objekt obj1= {init:‘Node’,version ‘18’} dhe kthen mesazhin “object from events: {init:‘Node’,version ‘18’}” . 
+krijo ne evnet tjeter “testEvent” ku kthen mesazhin “only test”. shfqaq numrin total te eventeve dhe ne fun fshij evnetin “testEvent”. Krijo nje repo te re, push 
+aplikacionin ne branch example-event-1 duke shtuar paraprakisht edhe .gitignore file.
+*/
 const EventEmmiter=require("events");
     const event=new EventEmmiter();
     // console.log(event);
@@ -28,3 +35,20 @@ const EventEmmiter=require("events");
     const count=event.listenerCount(eventNames);
     console.log("Total number of listeners"+`${eventNames} : ${count}`)
    })
+
+/*
+Pasi kemi  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; Te krijohet nje metod “getEvenNumbers” qe kthen vetem numrat qift. Te shfaqen ne eventin EvenEvent. 
+*/
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {}
+const myEmitter = new MyEmitter();
+
+function getEvenNumbers(numbers) {
+  const evenNumbers = numbers.filter(num => num % 2 === 0);
+  myEmitter.emit('EvenEvent', evenNumbers);
+  return evenNumbers;
+}
+
+getEvenNumbers(numbers);
